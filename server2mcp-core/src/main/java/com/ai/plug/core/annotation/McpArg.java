85f7a8703707af3758850e1fe8b8db3640368d37
TheEterna <125226601+TheEterna@users.noreply.github.com>
2025-07-20 13:56:46 +0800

    chore: update pom.xml

--- server2mcp-core/src/main/java/com/ai/plug/core/annotation/McpArg.java
+++ server2mcp-core/src/main/java/com/ai/plug/core/annotation/McpArg.java
@@ -12,7 +12,7 @@ import java.lang.annotation.*;
  * 在Tool里可以标识为mcp的参数, 比如exchange 和 上下文Map(由于类型擦除, 无法准确注入, 故使用该注解)
  * @author Christian Tzolov
  */
-@Target({ ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface McpArg {
